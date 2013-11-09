65bd8c6d1666864658ec963f331e041af78ea66c
kibitan <uzukifirst@gmail.com>
2013-11-10 00:19:39 +0900

    add init script

---
+++ init.sh
@@ -0,0 +1,12 @@
+#init shell
+## for git
+git config --add --global user.email uzukifirst@gmail.com
+git config --add --global user.name kibitan
+git config --add --global color.diff auto
+git config --add --global color.status auto
+git config --add --global color.branch auto
+git config --add --global alias.st status
+git config --add --global alias.ci commit
+git config --add --global alias.br branch
+git config --add --global alias.co checkout
+git config --global push.default simple
