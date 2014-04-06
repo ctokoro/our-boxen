e1bd5e2cb17b79236864feeb1d8d1492f15b9cff
ctokoro <uzukifirst@gmail.com>
2014-04-07 00:22:37 +0800

    add anvi(GUI pow)

--- modules/people/manifests/ctokoro.pp
+++ modules/people/manifests/ctokoro.pp
@@ -63,6 +63,10 @@ class people::ctokoro {
     'picasa':
       source => "https://dl.google.com/photos/picasamac39.dmg",
       provider => appdmg;
+    ## なんかエラる。。
+    # 'GUI pow Anvi':
+    #   source => "http://s3.amazonaws.com/sparkler_versions/versions/uploads/000/000/099/original/Anvil.app.zip?1377359559",
+    #   provider => compressed_app;      
   }
 
   # for dev  
