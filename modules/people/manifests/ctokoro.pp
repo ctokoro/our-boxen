f391c1442739ae6ba6a57b36895858932869d9fe
ctokoro <uzukifirst@gmail.com>
2014-05-10 02:00:24 +0530

    add flowdock

--- modules/people/manifests/ctokoro.pp
+++ modules/people/manifests/ctokoro.pp
@@ -66,7 +66,10 @@ class people::ctokoro {
     ## なんかエラる。。
     # 'GUI pow Anvi':
     #   source => "http://s3.amazonaws.com/sparkler_versions/versions/uploads/000/000/099/original/Anvil.app.zip?1377359559",
-    #   provider => compressed_app;      
+    #   provider => compressed_app;
+    'flowdock':
+      source => 'https://flowdock-resources.s3.amazonaws.com/mac/Flowdock.zip',
+      provider => compressed_app;
   }
 
   # for dev  
